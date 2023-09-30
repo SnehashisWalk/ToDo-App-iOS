1ab6d7731ba40e2c0375f75d41c339a9f90a7456
Snehashis Lenka <slenka321@gmail.com>
2023-09-30 13:59:23 -0400

    Initial Commit

---
+++ To-Do/To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoApp.swift
+//  To-Do
+//
+//  Created by Snehashis Lenka on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
